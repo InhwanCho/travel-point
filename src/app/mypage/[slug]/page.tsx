bc688fb4a40da6e1a7eb143c0cdb131104d9eff7
InhwanCho <whdlsghks2@gmail.com>
2024-05-23 14:49:50 +0900

    code refactoring

--- src/app/mypage/[slug]/page.tsx
+++ src/app/mypage/[slug]/page.tsx
@@ -12,7 +12,7 @@ interface MypageProps {
 }
 
 // 페이지네이션을 위한 데이터를 처리하는 함수
-export function usePaginatedData(data: any[], itemsPerPage: number) {
+function usePaginatedData(data: any[], itemsPerPage: number) {
   const [currentPage, setCurrentPage] = useState(1);
 
   const totalPages = Math.ceil(data.length / itemsPerPage);
