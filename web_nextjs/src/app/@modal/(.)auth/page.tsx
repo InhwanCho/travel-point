5f60283b6ccc6cd34452f57dce83dba9fe2dd5cf
InhwanCho <whdlsghks2@gmail.com>
2024-06-03 16:19:29 +0900

    deleted duplicate folder

--- web_nextjs/src/app/@modal/(.)auth/page.tsx
+++
@@ -1,8 +0,0 @@
-import AuthPage from '@/components/section/auth/authpage';
-import React from 'react';
-
-export default function InterceptPage() {
-  return (
-    <AuthPage />
-  );
-}
--- web_nextjs/src/types/region-type.ts
+++
@@ -1,4 +0,0 @@
-import { REGIONS } from "@/data/data";
-
-const regionNames = REGIONS.map((region) => region.name);
-export type RegionName = typeof regionNames[number] | 'all';
