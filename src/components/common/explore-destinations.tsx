b9c72f3b6a93c53a70102688fc85e001782eff88
InhwanCho <whdlsghks2@gmail.com>
2024-06-13 19:52:06 +0900

    tem array sort reverse

--- src/components/common/explore-destinations.tsx
+++ src/components/common/explore-destinations.tsx
@@ -122,7 +122,26 @@ export default function ExploreDestinations({
           <Separator />
         </>
       ) : page === 'festivals' ? ''
-        : page === 'regions' ? '' : ''
+        : page === 'regions' ? (
+          <>
+            <Separator />
+            <div className="flex h-5 items-center space-x-3 xsm:space-x-5 text-xs xsm:text-sm m-3 pl-1 list-none text-nowrap">
+              <li
+                onClick={() => handleThemeChange('all')}
+                className={`cursor-pointer font-medium ${pageColors.regions.ring} ring rounded-full ring-offset-2 px-1.5`}
+              >
+                전체
+              </li>
+              <li className={`font-medium rounded-full ring-offset-2 px-1.5`}>
+                후기순
+              </li>
+
+            </div>
+            <Separator />
+          </>
+        )
+
+          : ''
       }
 
       <section className="p-3 sm:p-6 grid grid-cols-2 xsm:grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-x-5 gap-y-8 pt-7 sm:pt-12">
